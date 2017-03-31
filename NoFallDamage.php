5d07c0cd183e23408459b24853880f0473fb4f8c
SinlesFlyer376 <J2ducky@gmail.com>
2017-03-31 16:21:40 -0500

    NoFall Created

---
+++ NoFallDamage.php
@@ -0,0 +1,30 @@
+<?php namespace NoFallingDamage;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\EntityDamageEvent;
+
+class Main extends PluginBase {
+  
+	public function onLoad() {
+		$this->getLogger()->info(TextFormat::WHITE . "NoFallDamage loaded!");
+	}
+  
+	public function onEnable() {
+		$this->getLogger()->info(TextFormat::DARK_GREEN . "NoFallDamage enabled!");
+	}
+  
+	public function onDisable() {
+		$this->getLogger()->info(TextFormat::DARK_RED . "NoFallDamage disabled!");
+	}
+	
+	public function onEntityDamage(EntityDamageEvent $event) {
+		$player = $event->getEntity();
+		if($player instanceof Player) {
+			if($event->getCause() != EntityDamageEvent::CAUSE_FALL) {
+				$event->setCancelled();
+			}
+		}
+	}
+}
